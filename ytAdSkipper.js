177698f955b2750b87ea678fe63a4cc0fe49fdd0
yashtikakakkar <yashtikakakkar@users.noreply.github.com>
2021-06-21 21:52:00 +0530

    changes updated

--- ytAdSkipper.js
+++ ytAdSkipper.js
@@ -1,33 +1,11 @@
-const puppeteer = require("puppeteer");
-
-let gBrowser;
-let gPage;
-
-(async function() {
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 50,
-        defaultViewport: null,
-        args: ["--start-maximized"],
-    });
-    gBrowser = browser;
-    let pagesArr = await browser.pages();
-    gPage = pagesArr[0];
-    await gPage.goto("https://www.youtube.com/watch?v=UYulBXIMikg");
-    await gPage.waitForSelector(".ytp-large-play-button.ytp-button");
-    await gPage.click(".ytp-large-play-button.ytp-button");
-
-    await gPage.evaluate(async function() {
-
-        let interval = setInterval(function() {
-            let overlayAd = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
-            if (overlayAd != undefined)
-                overlayAd.click();
-
-            let skipBtn = document.getElementsByClassName("ytp-ad-skip-button")[0];
-            if (skipBtn != undefined)
-                skipBtn.click();
-        }, 2000);
-
-    })
-})();
+// this function will check at regular intervals of 2 seconds if any ad has appeared, search for its class name, and click their respective close button
+setInterval(function () {
+  // for banner type overlay ads
+  let overlayAd = document.getElementsByClassName(
+    "ytp-ad-overlay-close-container"
+  )[0];
+  if (overlayAd != undefined) overlayAd.click();
+  // for proper ads whose skip button appears after 5 seconds
+  let skipBtn = document.getElementsByClassName("ytp-ad-skip-button")[0];
+  if (skipBtn != undefined) skipBtn.click();
+}, 2000);
