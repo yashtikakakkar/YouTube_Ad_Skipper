b387742cdd0758f660bfb6c7538c6e73244d08d6
Yashtika Kakkar <yashtika2000@gmail.com>
2021-06-30 19:01:00 +0530

    Update ytAdSkipper.js

--- ytAdSkipper.js
+++ ytAdSkipper.js
@@ -1,11 +1,16 @@
 // this function will check at regular intervals of 0.5 seconds if any ad has appeared, search for its class name, and click their respective close button
 setInterval(function () {
+  
   // for banner type overlay ads
-  let overlayAd = document.getElementsByClassName(
-    "ytp-ad-overlay-close-container"
-  )[0];
-  if (overlayAd != undefined) overlayAd.click();
+  let overlayAd = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
+  
+  if (overlayAd != undefined) 
+    overlayAd.click();
+  
   // for proper ads whose skip button appears after 5 seconds
   let skipBtn = document.getElementsByClassName("ytp-ad-skip-button")[0];
-  if (skipBtn != undefined) skipBtn.click();
+  
+  if (skipBtn != undefined) 
+    skipBtn.click();
+  
 }, 500);
